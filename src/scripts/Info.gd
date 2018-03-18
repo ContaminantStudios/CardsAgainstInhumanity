d816a6cd093eeee84cc49afb757f789b764704de
Noah Connors <noahconnors@mnmsa.org>
2018-03-18 11:21:01 -0500

    Add pages to the info screen

--- src/scripts/Info.gd
+++ src/scripts/Info.gd
@@ -1,14 +1,68 @@
 extends CanvasLayer
 
+var current_label
+var current_button
+
 func _ready():
-	$Button.disabled = true
+	$Back.disabled = true
+	current_label = 'Rules'
+	current_button = 'Next'
 
 func show_all():
 	$ColorRect.show()
-	$Label.show()
-	$Button.disabled  = false
+	show_current_label()
+	$Back.disabled = false
+	match current_button:
+		'Next':
+			$Next.disabled = false
+		'Prev':
+			$Previous.disabled = false
+	$EducationIcon.show()
+	$PoliticalIcon.show()
+	$MilitaryIcon.show()
 
 func hide_all():
 	$ColorRect.hide()
-	$Label.hide()
-	$Button.disabled = true
+	hide_current_label()
+	$Back.disabled = true
+	$Next.disabled = true
+	$Previous.disabled = true
+	$EducationIcon.hide()
+	$PoliticalIcon.hide()
+	$MilitaryIcon.hide()
+
+func next_pressed():
+	$Rules.hide()
+	$Controls.show()
+	current_label = 'Controls'
+	current_button = 'Prev'
+	$Next.disabled = true
+	$Previous.disabled = false
+	$EducationIcon.hide()
+	$PoliticalIcon.hide()
+	$MilitaryIcon.hide()
+
+func previous_pressed():
+	$Controls.hide()
+	$Rules.show()
+	current_label = 'Rules'
+	current_button = 'Next'
+	$Next.disabled = false
+	$Previous.disabled = true
+	$EducationIcon.show()
+	$PoliticalIcon.show()
+	$MilitaryIcon.show()
+
+func show_current_label():
+	match current_label:
+		'Rules':
+			$Rules.show()
+		'Controls':
+			$Controls.show()
+
+func hide_current_label():
+	match current_label:
+		'Rules':
+			$Rules.hide()
+		'Controls':
+			$Controls.hide()
